ebd4dc565d43f5cb1104550aadef8a603c5e8acb
Scott Sadler <scott@alphanode.co>
2017-08-02 23:38:12 +0000

    changes

--- silence.py
+++ silence.py
@@ -2,23 +2,22 @@ import subprocess
 import time
 import RPi.GPIO as GPIO
 
+GPIO_CHANNEL = 26
 
 # $ adduser root pulse-access
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.OUT)
-GPIO.output(26, 0)
-
-silence = True
-delay = 5
-lastamp = 0
-
+GPIO.setup(GPIO_CHANNEL, GPIO.OUT)
 
 arecord = subprocess.Popen(
         ['arecord', '-D', 'pulse', '-q', '-r', '2000'],
         stdout=subprocess.PIPE)
 
+time.sleep(1)
 print repr(arecord.stdout.read(100))
 
+silence = GPIO.input(GPIO_CHANNEL)
+delay = 20
+lastamp = time.time()
 
 while True:
     t = time.time()
@@ -27,10 +26,10 @@ while True:
     if amp > 1:
         if silence:
             silence = False
-            GPIO.output(26, 0)
+            GPIO.output(GPIO_CHANNEL, 0)
         lastamp = t
     else:
         if not silence:
             if t - lastamp > delay:
                 silence = True
-                GPIO.output(26, 1)
+                GPIO.output(GPIO_CHANNEL, 1)
